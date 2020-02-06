81a2063509152abb32f4c0d7f767a1cb5d5cc9d2
SE <se@localhost.localdomain>
2020-02-06 12:39:53 +0530

    mpl 6-2-20

--- MPL/assign-3.asm
+++ MPL/assign-3.asm
@@ -25,18 +25,19 @@ section .data
 	db "1. HEX to BCD", 0xA,
 	db "2. BCD to HEX", 0xA,
 	db "0. Exit", 0xA,
-	db " Select an option : ", 0xA
+	db " Select an option : ",
 	lenmenu equ $-menu
 	option db 2
 
 	digits db 4
-	number db 4
-	indigs db 1
-	bcdhex dw 1
+	bcddigits db 0
+	hexaval dw 1
 	index db 0
-	
 	nl: db 0xA
 
+section .bss
+	number resb 4
+	result resb 1
 section .text
 
 global _start
@@ -54,25 +55,35 @@ _start:
 	jz case3
 ;-------------------------all option cases-------------------
 case1:
-	input number, [digits]
-	mov cl, [indigs]
-	mov rax, number
-	call atoh
-	mov [bcdhex], ax
-	call htob
-	
+	input number, 5
+	mov cl, 4
+	mov rsi, number
+	call asciitohex
+	mov [hexaval], ax
+	call hextobcd
+	exit
 	
 
 case2:
+	input number, 5
+	mov cl, 4
+	mov rsi, number
+	call bcdtohex
+	;mov [hexaval], ax
+	call hextoascii
+	mov rsi, number
+	mov cl, 4	
+	call printarr
+
 case3:	exit
 
-
-atoh:
+;convert axcii values to hexa
+asciitohex:
 	xor ax, ax
 	xor bx, bx
 atohloop:
 	rol ax, 4
-	mov bl, byte[rax]
+	mov bl, byte[rsi]
 	cmp bl, 39h
 	jbe sub30
 	sub bl, 07h
@@ -84,18 +95,72 @@ sub30:
 	jnz atohloop	
 ret	
 
-htob:
-	mov ax, [bcdhex]
+;convert hexavalues in bcdhex to its corresponding bcd
+hextobcd:
+
+	mov ax, [hexaval]
+	mov bx, 0xA
+htobloop:
+	xor edx, edx
+	div bx
+	push dx	
+	inc byte[bcddigits]
+	cmp ax, 0
+	jnz htobloop
 	
-	
+poploop:	
+	pop dx
+	add dx, 30h
+	mov [result], dx
+	print result, 1
+	dec byte[bcddigits]
+	jnz poploop	
 ret
 
+;convert entered bcd values to hexa values
+bcdtohex:
+	xor ax, ax
+	xor bx, bx
+	xor dx, dx
 
+	mov bx, 0xA
+btohloop:
+	mul bx
+	mov dl, byte[rsi]
+	sub dl, 30h
+	add ax, dx
+	inc rsi
+	dec cl
+	jnz btohloop
+ret
 
+hextoascii:
+	mov rsi, number
+	mov cl, 4
+	htoaloop:
+		rol ax, 04
+		mov bl, al
+		and bl, 0xF
+		cmp bl, 09
+		jbe x
+		add bl, 07
+	x:	add bl, 30h
+		mov byte[rsi], bl
+		inc rsi
+		dec cl
+		jnz htoaloop	
+ret
 
+printarr:
 
-
-
+printarrloop:
+	mov dl, byte[rsi]
+	mov [result], dl
+	print result, 1
+	inc rsi
+	dec cl
+	jnz printarrloop	
+ret
 
 
 
